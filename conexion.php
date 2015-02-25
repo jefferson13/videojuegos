135321adae1493d86178377f5a79b61b31879a7e
jefferson13 <jefferson_roa@hotmail.es>
2015-02-24 20:40:31 -0500

    modificando conecion.php

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php 
 /*conection: 
-
+$conexion = mysqli_connect("localhost","root","","juegos") or die("Error " . mysqli_error($conexion));
 
 //consultation: 
 
@@ -10,13 +10,29 @@ $query = "SELECT name FROM mytable" or die("Error in the consult.." . mysqli_err
 
 $result = mysqli_query($link, $query); 
 
+
+
 //display information: 
 
 while($row = mysqli_fetch_array($result)) { 
-  echo $row["name"] . "<br>"; 
-} */
-$Buscar=$_POST["Buscar"];
-$link = mysqli_connect("localhost","root","","juegos") or die("Error " . mysqli_error($link)); 
-echo "Tu nombre es : ".$Buscar;
+  echo $row["name"] . "<br>";
+
+}
+$Buscar=$_POST["Buscar"]; 
+*/
+
+$nombre=$_POST["nombre"];
+$cedula=$_POST["cedula"];
+$telefono=$_POST["telefono"];
+$conexion = mysqli_connect("localhost","root","","juegos") or die("Error " . mysqli_error($conexion)); 
+$consulta="INSERT INTO Cliente VALUES('$nombre',$cedula,$telefono)";
+$insertar=mysqli_query($conexion,$consulta);
+if($insertar){
+	echo "Insertado con exito";
+}
+else{
+	echo "insercion fallida";
+}
+
 ?> 
 
