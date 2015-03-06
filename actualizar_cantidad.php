9a0e486e9601f4a561d701be2980aba165edea3f
jefferson13 <jefferson_roa@hotmail.es>
2015-03-05 21:34:24 -0500

    proyecto PSPII

--- actualizar_cantidad.php
+++ actualizar_cantidad.php
@@ -1,9 +1,23 @@
 <?php
-foreach ($_POST['probando'] as $nombre ){
-	include "conexion.php";
-    $consulta="update juego set cantidad=(cantidad-1) where nombre='$nombre'";
-    $filas= mysqli_query($conexion, $consulta);
-    }
+$nombre=$_POST['nombre'];
+$cliente=$_POST['cliente'];
+$precio=$_POST['precio'];
+$fecha_entrega=$_POST['fecha'];
+$id_alquiler=mktime();
+$fecha_inicio=date('Y-m-j');
+
+include "conexion.php";
+$consulta="INSERT INTO  alquiler VALUES('$id_alquiler', '$fecha_inicio', '$fecha_entrega', $precio,'$nombre',$cliente)";
+$insertar=mysqli_query($conexion,$consulta);
+
+$consulta="update juego set cantidad=(cantidad-1) where nombre='$nombre'";
+$actualizar= mysqli_query($conexion, $consulta);
+header("Location:juegos.php");
+
+
+?>	
+	
+	
     
 
-?>
+
